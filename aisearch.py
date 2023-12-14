1b3fc60ba53cdf6eca76abbc9ca062993b505f3c
Henry Chen <cbhenry@163.com>
2023-12-14 22:20:13 +0800

    accept hate speech category provide smooth answer also, we are polit.

--- aisearch.py
+++ aisearch.py
@@ -1,19 +1,43 @@
 import google.generativeai as genai
 import streamlit as st
+import json
 
-st.title("Google AI Powered")
+print('* Enter the application')
+
+st.title("Google AI Powered Search")
+st.subheader("BETA-Version, bugs 🐞 are accepted.")
+st.subheader("Drop message to :blue[Henry] (_if you know him_) if met 🐞🐛.", divider="rainbow")
 
 genai.configure(api_key=st.secrets["AITK"])
-model = genai.GenerativeModel(model_name='gemini-pro')
+model = genai.GenerativeModel(model_name='gemini-pro', safety_settings=[
+        {
+            "category": "HARM_CATEGORY_HARASSMENT",
+            "threshold": "BLOCK_NONE",
+        },{
+            "category": "HARM_CATEGORY_SEXUAL",
+            "threshold": "BLOCK_NONE",
+        },{
+            "category": "HARM_CATEGORY_HATE_SPEECH",
+            "threshold": "BLOCK_NONE",
+        }
+        ]
+        )
+
+print("* constructed model")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+print("* init messages")
+
 for message in st.session_state.messages:
+    print("** check messages")
     with st.chat_message(message["role"]):
+        print("*** chat messages")
         st.markdown(message["parts"])
 
 if prompt := st.chat_input("What is up?"):
+    print(prompt)
     st.session_state.messages.append({"role": "user", "parts": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
@@ -28,6 +52,7 @@ if prompt := st.chat_input("What is up?"):
             ],
             stream=True,
         ):
+            print(response.__dict__)
             full_response += (response.text or "")
             message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
