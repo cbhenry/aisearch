f0d3f11a899ec784950baeb08c2a737bf2e7b781
Henry Chen <cbhenry@163.com>
2025-02-09 17:48:40 +0800

    Add support of deepseek

--- aisearch.py
+++ aisearch.py
@@ -1,12 +1,14 @@
 import google.generativeai as genai
+from openai import OpenAI
 import streamlit as st
 import json
 
 print('* Enter the application')
 
-st.title("Google AI Powered Search")
-st.subheader("BETA-Version, bugs 🐞 are accepted.")
-st.subheader("Drop message to :blue[Henry] (_if you know him_) if met 🐞🐛.", divider="rainbow")
+st.title("AI Powered Search - for those who being blocked")
+st.subheader("Lots of bugs, dont expected to much, donate to extend the tokens.")
+st.image("alipay.png", width=100)
+st.write("Drop message to :blue[Henry] (_if you know him_) if met 🐞🐛.", divider="rainbow")
 
 genai.configure(api_key=st.secrets["AITK"])
 model = genai.GenerativeModel(model_name='gemini-pro', safety_settings=[
@@ -23,6 +25,8 @@ model = genai.GenerativeModel(model_name='gemini-pro', safety_settings=[
         ]
         )
 
+model2 = OpenAI(api_key=st.secrets["AITK2"], base_url="https://api.deepseek.com")
+
 print("* constructed model")
 
 if "messages" not in st.session_state:
@@ -36,24 +40,53 @@ for message in st.session_state.messages:
         print("*** chat messages")
         st.markdown(message["parts"])
 
+option = st.selectbox(
+    "Select AI Model:",
+    ("Google Gemini", "DeepSeek-Chat", "DeekSeek-Reasoner"),
+)
+
 if prompt := st.chat_input("What is up?"):
     print(prompt)
     st.session_state.messages.append({"role": "user", "parts": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
 
-    with st.chat_message("model"):
+    with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
-        for response in model.generate_content(
-            [
-                {"role": m["role"], "parts": m["parts"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        ):
-            print(response.__dict__)
-            full_response += (response.text or "")
-            message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
-    st.session_state.messages.append({"role": "model", "parts": full_response})
+        try:
+            if option == "Google Gemini":
+                response = model.generate_content(prompt)
+                print(response.text)
+                full_response = response.text
+            elif option == "DeepSeek-Chat":
+                completion = model2.chat.completions.create(
+                    model="deepseek-chat",
+                    messages=[
+                        {"role": m["role"], "content": m["parts"]}
+                        for m in st.session_state.messages
+                    ],
+                    stream=True,
+                )
+                for chunk in completion:
+                    if chunk.choices[0].delta.content is not None:
+                        full_response += chunk.choices[0].delta.content
+                        message_placeholder.markdown(full_response + "▌")
+            elif option == "DeekSeek-Reasoner":
+                completion = model2.chat.completions.create(
+                    model="deepseek-coder",
+                    messages=[
+                        {"role": m["role"], "content": m["parts"]}
+                        for m in st.session_state.messages
+                    ],
+                    stream=True,
+                )
+                for chunk in completion:
+                    if chunk.choices[0].delta.content is not None:
+                        full_response += chunk.choices[0].delta.content
+                        message_placeholder.markdown(full_response + "▌")
+            message_placeholder.markdown(full_response)
+            st.session_state.messages.append({"role": "assistant", "parts": full_response})
+        except Exception as e:
+            print(e)
+            st.toast(e, icon='🎉')
